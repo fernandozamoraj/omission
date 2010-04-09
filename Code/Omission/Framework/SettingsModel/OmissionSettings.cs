7c71f0b6c9feaf2d9a129c79a97db7304d40ba6f
Fernando.Zamora.Jimenez <Fernando.Zamora.Jimenez@dea76284-6adb-0356-79ec-0946b1e83c87>
2010-04-09 09:35:20 +0000

    Made changes... mostly comments to consider somethings for settins

---
+++ Code/Omission/Framework/SettingsModel/OmissionSettings.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Omission.Framework.SettingsModel
+{
+    /// <summary>
+    /// Cant think of what would make a good settings variable
+    /// At first I thought you could map certain exceptions to certain
+    /// Handlers but then that would mean that you would have to have intimate
+    /// knowledge of the Omission Framework
+    /// Then I thought well.. what about logging... we could map certain loggers
+    /// with certain exceptions but that is more of a logging issues
+    /// the default logger would have to handle that
+    /// ...
+    /// that comes to mind is that of silencing certain exceptions
+    /// Other ideas are possibly exposing certain exceptions
+    /// Or silencing all exceptions or exposing all exceptions
+    /// what about exposing the entire stack for certain exceptions
+    /// ...
+    /// This is put on hold until I can decide what the proper settings are
+    /// ...
+    /// </summary>
+    public class OmissionSettings
+    {
+        public static OmissionSettings FromFile(string filePath)
+        {
+            OmissionSettings omissionSettings;
+
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                omissionSettings = FromXmlString(streamReader.ReadToEnd());
+            }
+
+            return omissionSettings;
+        }
+
+        public static OmissionSettings FromXmlString(string xmlString)
+        {
+            XDocument document = XDocument.Parse(xmlString);
+
+            OmissionSettings omissionSettings = new OmissionSettings();
+            
+            omissionSettings.LoadFromXml(document.Element("ProductGraph"));
+
+            return omissionSettings;
+        }
+
+        public void LoadFromXml(XElement element)
+        {
+            
+        }
+
+        public List<string> ProduceXml()
+        {
+            return new List<string>();
+        }
+    }
+}
